f8cc2d00cf53c2110eac85ef4bb8bbcafe0154d1
Lakshya Nagpal <lnagpal15@gmail.com>
2026-06-27 20:33:16 +0530

    Create 9-PalindromeNumber

---
+++ SQL/9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -0,0 +1,17 @@
+// Last updated: 6/27/2026, 8:33:15 PM
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        long rev=0;
+        int temp=x;
+        while(temp)
+        {
+            int digit=temp%10;
+            rev=rev*10+digit;
+            temp/=10;
+        }
+        if(rev==x) return true;
+        return false;
+    }
+};
