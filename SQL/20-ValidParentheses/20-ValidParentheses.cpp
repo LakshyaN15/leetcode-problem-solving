dc5eb1b9f3665c2210a693e9774ae1839e73bfef
Lakshya Nagpal <lnagpal15@gmail.com>
2026-06-27 20:33:10 +0530

    Create 20-ValidParentheses

---
+++ SQL/20-ValidParentheses/20-ValidParentheses.cpp
@@ -0,0 +1,19 @@
+// Last updated: 6/27/2026, 8:33:09 PM
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(char c:s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            } else {
+        if (st.empty() || (c== ')' && st.top()!= '(') || (c== '}'            
+        && st.top()!= '{') || (c== ']' && st.top()!= '[')) {
+                    return false;
+                }
+            st.pop();
+            }
+        }
+    return st.empty();
+    }
+};
