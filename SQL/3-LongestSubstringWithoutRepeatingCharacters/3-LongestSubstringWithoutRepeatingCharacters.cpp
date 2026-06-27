70804c3d70b4e60a5dfbdfcbe12153907e1494b5
Lakshya Nagpal <lnagpal15@gmail.com>
2026-06-27 20:33:22 +0530

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ SQL/3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,20 @@
+// Last updated: 6/27/2026, 8:33:21 PM
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> mp(256,-1);
+        int n=s.size();
+        int left=0;
+        int right=0;
+        int len=0;
+        while(right<n){
+            if(mp[s[right]]!=-1)
+            left=max(mp[s[right]]+1,left);
+            mp[s[right]]=right;
+            len=max(len,right-left+1);
+            right++;
+        }
+        return len;
+        
+    }
+};
