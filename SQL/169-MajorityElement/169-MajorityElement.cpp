77cacd1eea996181e0d96354bf5a1166be44f1ee
Lakshya Nagpal <lnagpal15@gmail.com>
2026-06-27 20:32:46 +0530

    Create 169-MajorityElement

---
+++ SQL/169-MajorityElement/169-MajorityElement.cpp
@@ -0,0 +1,18 @@
+// Last updated: 6/27/2026, 8:32:44 PM
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       int n=nums.size();
+       unordered_map<int,int> mp;
+       for(int i=0; i<n; i++){
+           mp[nums[i]]++;
+       }
+         n=n/2;
+       for(auto i:mp){
+           if(i.second>n){
+               return i.first;
+           }
+       }
+       return 0;
+    }
+};
