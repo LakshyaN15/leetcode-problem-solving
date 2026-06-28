7ba06537fe2d5387e1267996571408b3f02d2fae
“Lakshya <lnagpal15@gmail.com>
2026-06-29 00:42:44 +0530

    Added logic

--- Algorithms/math/reverse-integer.py
+++ Algorithms/math/reverse-integer.py
@@ -1,10 +1,16 @@
-# Title: Reverse Integer
+# Title: 1. Reverse Integer
 # Link: https://leetcode.com/problems/reverse-integer/
 # Difficulty: Medium
 # Topics: Math
 # TC: O(n)
 # SC: O(n)
-# Approach: No
+# Approach: 1. Brute Force - initialize 2 var I and j to itr and check if nums[i]==nums[j] return true return false end;
+# TC = O(n^2) SC=O(n)
+# 2. Sorting - sort using nums.sort() and compare nums[i]==nums[i-1]
+# TC=O(nlogn) SC=O(1) or O(n)
+# 3. Hashset = declare a set and initialize another var num, for loop num in nums and if condition, if num is present in seen then true , if not add and in the end return false.
+# TC= O(n)
+# SC=O(n)
 
 class Solution(object):
     def reverse(self, x: int) -> int:
