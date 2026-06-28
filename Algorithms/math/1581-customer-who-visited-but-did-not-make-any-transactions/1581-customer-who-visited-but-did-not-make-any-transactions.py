4bae44a59586233e13703cdfe85c21ff08a57d14
Lakshya Nagpal <lnagpal15@gmail.com>
2026-06-28 23:28:31 +0530

    Deleted Junk

--- Algorithms/math/1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
-    merged = pd.merge(visits, transactions, on='visit_id', how='left')
-    result = merged[merged['transaction_id'].isna()].groupby('customer_id', as_index=False)['visit_id'].count()
-    result.rename(columns={'visit_id': 'count_no_trans'}, inplace=True)
-    return result
-
-    
