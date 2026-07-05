e36ff72fdefc480c6476ccc065b66a3331dde4c7
Lakshya Nagpal <lnagpal15@gmail.com>
2026-07-05 16:55:42 +0530

    Time: 6 ms (61.27%) | Memory: 21.2 MB (19.42%) - LeetSync

--- Algorithms/recursion-and-backtracking/169-majority-element/majority-element.py
+++ Algorithms/recursion-and-backtracking/169-majority-element/majority-element.py
@@ -18,6 +18,7 @@
 # Approach: 1. Brute Force -> Using nested loop or count=sum(1 for i in nums if i==num), adds 1 for every element found and > 1 (Time Limit Exceeded)
 # TC: O(n^2)
 # SC: O(1)
+
 # class Solution:
 #     def majorityElement(self, nums: List[int]) -> int:
 #         n=len(nums)
@@ -29,11 +30,11 @@
 # Approach: 2. Hash Map -> declare a defaultdict(int) - a special dictionary that auto-creates missing keys with a default value — and for int, that default is 0. This means you can do count[num] += 1 directly, even the very first time you see num, without ever getting a KeyError, delacre maxCount=res=0 and check if maxCount<count[num], if yes then res=num and maxCount=count[num]. Return max appeared num in end as res
 # TC: O(n)
 # SC: O(n)
+
 # class Solution:
 #     def majorityElement(self, nums: List[int]) -> int:
 #         count=defaultdict(int)
 #         res=maxCount=0
-
 #         for num in nums:
 #             count[num]+=1
 #             if maxCount < count[num]:
@@ -44,6 +45,7 @@
 # Approach: 3. Sorting -> Approach is to sort as after sort majority element will occupy middle pos no matter what.
 # TC: O(nlogn)
 # SC: O(1) or O(n) depending on sorting algo
+
 # class Solution:
 #     def majorityElement(self, nums: List[int]) -> int:
 #         nums.sort()
@@ -53,6 +55,7 @@
 # Approach: 4. Booye Moore -> The Boyer-Moore algorithm works by maintaining a candidate and a count. When we see the candidate, we increment the count; otherwise, we decrement it. When the count reaches 0, we pick a new candidate. Since the majority element appears more than half the time, it will survive this elimination process and remain as the final candidate.
 # TC: O(n)
 # SC: O(1)
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         res=count=0
