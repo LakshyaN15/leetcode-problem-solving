e6601e27353050d3ff6a3462b5992c72ff71f0c5
Lakshya Nagpal <25548101+LakshyaN15@users.noreply.github.com>
2026-07-14 10:16:21 +0530

    Refactor SQL query for managers with 5+ direct reports
    Updated SQL query to use a subquery for counting direct reports.

--- SQL/570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ SQL/570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,2 +1,6 @@
-# Write your MySQL query statement below
-select e.name from Employee e join Employee e2 on e.id=e2.managerId group by e2.managerId having count(*)>=5
+# Approach: Joins
+-- select e.name from Employee e join Employee e2 on e.id=e2.managerId group by e2.managerId having count(*)>=5
+
+# Approach: Subquery, Joins
+select e.name from Employee e join (select managerId, count(*) as directReports from Employee group by managerId having count(*)>=5)
+e2 on e.id=e2.managerId
