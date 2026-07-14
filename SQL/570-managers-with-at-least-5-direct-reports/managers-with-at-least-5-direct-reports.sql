99caac3c25a96cf23f3e6f8a2ee33a59a00c7f49
Lakshya Nagpal <25548101+LakshyaN15@users.noreply.github.com>
2026-07-14 10:17:00 +0530

    Update managers-with-at-least-5-direct-reports.sql

--- SQL/570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ SQL/570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,5 @@
-# Approach: Joins
+# Approach: Subquery, Joins
 -- select e.name from Employee e join Employee e2 on e.id=e2.managerId group by e2.managerId having count(*)>=5
 
-# Approach: Subquery, Joins
 select e.name from Employee e join (select managerId, count(*) as directReports from Employee group by managerId having count(*)>=5)
 e2 on e.id=e2.managerId
