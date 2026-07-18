b344f6647c7f390a72bd5dce39800ccec9c70f66
Lakshya Nagpal <25548101+LakshyaN15@users.noreply.github.com>
2026-07-18 22:09:19 +0530

    Update queries-quality-and-percentage.sql

--- SQL/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ SQL/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 # Approach: ROUND, CAST, CASE WHEN THEN 1 ELSE 0 END, SUM, IF, GROUP BY, IS NOT NULL
--- select query_name, ROUND(avg(cast(rating as decimal)/position),2) as quality, ROUND(sum(case when rating<3 then 1 else 0 end)*100/count(*),2) as poor_query_percentage from Queries where query_name is not null group by query_name
+-- select query_name, ROUND(avg(cast(rating as decimal)/position),2) as quality, ROUND(sum(case when rating<3 then 1 else 0 end)*100/count(*),2) as poor_query_percentage from Queries where query_name is not null group by query_name --(Optimized)
 select query_name, round(avg(rating/position),2) as quality, round(avg(if(rating<3,1,0))*100,2) as poor_query_percentage from Queries group by query_name
