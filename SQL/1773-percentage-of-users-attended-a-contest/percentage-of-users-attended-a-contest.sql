5b8189851cf598d11c45568e8d8734b7d488f4f0
Lakshya Nagpal <25548101+LakshyaN15@users.noreply.github.com>
2026-07-18 20:48:25 +0530

    Update percentage-of-users-attended-a-contest.sql

--- SQL/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ SQL/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-# Approach: ROUND, COUNT, GROUP BY , ORDER BY DESC and ASC
+# Approach: ROUND, COUNT, GROUP BY , ORDER BY DESC and ASC, Subquery
 select contest_id , ROUND((COUNT(user_id)/(select count(1) from users) *100),2) as percentage from Register group by contest_id order by percentage desc,contest_id;
