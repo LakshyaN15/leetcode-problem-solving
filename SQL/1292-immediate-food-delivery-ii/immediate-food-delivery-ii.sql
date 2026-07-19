7304cea404e2f709b13ad668041550bb07c692d3
Lakshya Nagpal <25548101+LakshyaN15@users.noreply.github.com>
2026-07-19 17:43:34 +0530

    Time: 586 ms (95.51%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ SQL/1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-# Approach: ROUND, AVG, Subquery, MIN
+# Approach: ROUND, AVG, Subquery, MIN, Count
 -- select ROUND(AVG(order_date=customer_pref_delivery_date)*100,2) as immediate_percentage from Delivery where (customer_id,order_date) in (select customer_id,min(order_date) from Delivery group by customer_id)
  select ROUND(SUM(IF(order_date=customer_pref_delivery_date,1,0))*100/count(delivery_id),2) as immediate_percentage from Delivery where (customer_id,order_date) in (select customer_id,min(order_date) from Delivery group by customer_id)
